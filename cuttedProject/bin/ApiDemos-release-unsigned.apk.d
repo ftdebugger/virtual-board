68bf066f8d3eb85c816e0a30a1389cd782f89da1
nchereva <nchereva@gmail.com>
2013-06-17 16:31:13 +0000

    FingerPaint - main activity, clear all implemented.

--- cuttedProject/bin/ApiDemos-release-unsigned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/mikalai_charavatsenka/Documents/ApiDemos/bin/ApiDemos-release-unsigned.apk : \
-/Users/mikalai_charavatsenka/Documents/ApiDemos/bin/ApiDemos.ap_ \
-/Users/mikalai_charavatsenka/Documents/ApiDemos/bin/classes.dex \
-/Users/mikalai_charavatsenka/Documents/ApiDemos/libs/alljoyn.jar \
-/Users/mikalai_charavatsenka/Documents/ApiDemos/libs/armeabi/liballjoyn_java.so \
